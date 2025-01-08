d0cef8dce5c6936cd522ac0909593fb4b50b989a
Maxence Cholet <153302107+Toblek@users.noreply.github.com>
2025-01-08 15:00:03 +0100

    Pause Menu, à pofiner

--- Assets/ScriptNathan/MenuManager.cs
+++ Assets/ScriptNathan/MenuManager.cs
@@ -1,10 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject pauseMenuCanvas;
+    private bool isPaused = false;
+
+    private void Start()
+    {
+        pauseMenuCanvas.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("nathan");
