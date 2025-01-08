6795a4d632129cf30be9d24ad254889ad614153e
xoNora <karell.splingart@gmail.com>
2025-01-08 03:42:58 +0100

    ToggleActionWheel

---
+++ Assets/ToggleActionWheel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleActionWheel : MonoBehaviour
+{
+    public GameObject actionWheelCanvas;
+
+    private bool isActionWheelOpen = false;
+
+    void Start()
+    {
+        actionWheelCanvas.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            toggleActionWheel();
+        }
+    }
+
+    void toggleActionWheel()
+    {
+        isActionWheelOpen = !isActionWheelOpen;
+
+        actionWheelCanvas.SetActive(isActionWheelOpen);
+    }
+}
